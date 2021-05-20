2c2bfc2b2bc0a0b78bc4f9ef45e2d7d0893c24e4
Maciej Karczewski <m.karczewski2001@gmail.com>
2021-05-20 12:53:34 +0200

    dodanie tła

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import arcade
 import os
+from game_variables import *
 
 class Game(arcade.Window):
 
@@ -9,10 +10,28 @@ class Game(arcade.Window):
         super().__init__(width, height, title="Flappy Birds")
 
         self.birds = None
+        # Background texture
+        self.background = None
+
+    def setup(self):
+        self.background = arcade.load_texture(BACKGROUNDS[0])
+
+    def draw(self):
+        """Funcion draw background"""
+        arcade.draw_texture_rectangle(self.width // 2, self.height // 2, self.width, self.height, self.background, alpha=200 )
+
+    def on_draw(self):
+        """ This is the method called when the drawing time comes."""
+
+        #render all objects
+        arcade.start_render()
+
+        self.draw()
 
 def run_game():
     os.chdir(os.path.split(os.path.realpath(__file__))[0])
-    game = Game(576, 712)
+    game = Game(GAME_WIDTH, GAME_HEIGHT )
+    game.setup()
     arcade.run()
 
 if __name__ == "__main__":
---
+++ game_variables.py
@@ -0,0 +1,5 @@
+from os import sep
+
+GAME_WIDTH = 480 * 2
+GAME_HEIGHT = 322 * 2
+BACKGROUNDS = ["asserts" + sep + "Images" + sep + "background1.png"]
