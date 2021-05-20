2080cb94f2a76e6339eeb7cf868297acc2dc3712
Maciej Karczewski <m.karczewski2001@gmail.com>
2021-05-20 14:08:00 +0200

    dodanie ptaka

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,16 +9,18 @@ class Game(arcade.Window):
         """ Inite game window"""
         super().__init__(width, height, title="Flappy Birds")
 
-        self.birds = None
+        self.bird = None
         # Background texture
         self.background = None
 
     def setup(self):
         self.background = arcade.load_texture(BACKGROUNDS[0])
+        self.bird = arcade.load_texture(BIRDS[0])
 
     def draw(self):
         """Funcion draw background"""
         arcade.draw_texture_rectangle(self.width // 2, self.height // 2, self.width, self.height, self.background, alpha=200 )
+        arcade.draw_scaled_texture_rectangle(self.width // 5, self.height // 2, self.bird, scale=0.4)
 
     def on_draw(self):
         """ This is the method called when the drawing time comes."""
@@ -27,6 +29,12 @@ class Game(arcade.Window):
         arcade.start_render()
 
         self.draw()
+
+    def update(self, dt):
+        pass
+
+
+
 
 def run_game():
     os.chdir(os.path.split(os.path.realpath(__file__))[0])
--- game_variables.py
+++ game_variables.py
@@ -3,3 +3,4 @@ from os import sep
 GAME_WIDTH = 480 * 2
 GAME_HEIGHT = 322 * 2
 BACKGROUNDS = ["asserts" + sep + "Images" + sep + "background1.png"]
+BIRDS = ["asserts" + sep + "Images" + sep + "bird1.png"]
